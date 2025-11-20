600dd6c958c21b3d721fdc484acccce2174551c4
ooileiko-cmyk <o.o.ileiko@student.khai.edu>
2025-11-20 17:26:50 +0200

    Stage 1: Add friends for dinner party

---
+++ dinnerparty/dinnerparty.py
@@ -0,0 +1,11 @@
+num_friends = int(input("Enter the number of friends joining (including you):\n> "))
+
+if num_friends <= 0:
+    print("No one is joining for the party")
+else:
+    friends = {}
+    print("Enter the name of every friend (including you), each on a new line:")
+    for _ in range(num_friends):
+        name = input("> ")
+        friends[name] = 0
+    print(friends)
