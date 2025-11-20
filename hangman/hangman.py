47a9feca6dd1b623e3e4c449cd8461da7d328985
ooileiko-cmyk <o.o.ileiko@student.khai.edu>
2025-11-20 09:34:21 +0200

    Stage 1: Add game announcement

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,12 +1,4 @@
+import random
+
 print("HANGMAN")
 print("The game will be available soon.")
-print("HANGMAN")
-
-secret_word = "python"
-
-guess = input("Guess the word: > ")
-
-if guess == secret_word:
-    print("You survived!")
-else:
-    print("You lost!")
