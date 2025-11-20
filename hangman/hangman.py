bc47f1af8e54d316fc7abb8dce2b602a87f9d276
ooileiko-cmyk <o.o.ileiko@student.khai.edu>
2025-11-20 09:01:49 +0200

    Stage 2: Simple version of the game

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,2 +1,12 @@
 print("HANGMAN")
 print("The game will be available soon.")
+print("HANGMAN")
+
+secret_word = "python"
+
+guess = input("Guess the word: > ")
+
+if guess == secret_word:
+    print("You survived!")
+else:
+    print("You lost!")
