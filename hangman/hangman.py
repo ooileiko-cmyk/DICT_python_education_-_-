c3338c013c8cb4029f0c9a6170590aff0ec77f4d
ooileiko-cmyk <o.o.ileiko@student.khai.edu>
2025-11-20 09:39:45 +0200

    Stage 3: Random word from list

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -4,3 +4,8 @@ print("HANGMAN")
 print("The game will be available soon.")
 words = ["python", "java", "javascript", "php"]
 secret_word =random.choice(words)
+guess_word = input("Guess the word: > ")
+if guess_word == secret_word:
+    print("You survived!")
+else:
+    print("You lost!")
