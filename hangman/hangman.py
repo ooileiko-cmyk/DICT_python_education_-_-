3bcee7938289f1fce347f81930a04e6b4d726619
ooileiko-cmyk <o.o.ileiko@student.khai.edu>
2025-11-20 09:41:44 +0200

    Stage 4: Show first 3 letters and input a letter

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -9,3 +9,11 @@ if guess_word == secret_word:
     print("You survived!")
 else:
     print("You lost!")
+    display_word = secret_word[:3] + "-" * (len(secret_word) - 3)
+    print("Guess the word:", display_word)
+
+    guess_letter = input("Input a letter: > ")
+    if guess_letter in secret_word:
+        print("Correct!")
+    else:
+        print("That letter doesn't appear in the word")
