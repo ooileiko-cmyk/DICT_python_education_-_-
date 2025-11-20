33651ffb372421d22f454e7010cf3ada34e81d45
ooileiko-cmyk <o.o.ileiko@student.khai.edu>
2025-11-20 16:54:17 +0200

    Finish hangman game

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,58 +1,56 @@
 import random
 
-print("HANGMAN")
-print("The game will be available soon.")
+def play_game():
+    print("HANGMAN")
 
-words = ["python", "java", "javascript", "php"]
-secret_word =random.choice(words)
-
-guess_word = input("Guess the word: > ")
-if guess_word == secret_word:
-    print("You survived!")
-else:
-    print("You lost!")
-
-    display_word = secret_word[:3] + "-" * (len(secret_word) - 3)
-    print("Guess the word:", display_word)
-
-    guess_letter = input("Input a letter: > ")
-    if guess_letter in secret_word:
-        print("Correct!")
-    else:
-        print("That letter doesn't appear in the word")
+    words = ["python", "java", "javascript", "php"]
+    secret_word = random.choice(words)
+    display_word = "-" * len(secret_word)
     attempts = 8
     guessed_letters = set()
 
-    while attempts > 0 and "-" in display_word:
-        guess_letter = input("Input a letter: > ")
+    while attempts > 0 and display_word != secret_word:
+        print(display_word)
+        guess = input("Input a letter: > ")
 
-        if len(guess_letter) != 1:
-            print("You should input a single letter")
+        if len(guess) != 1:
+            print("Please, input a single letter.")
+            continue
+        if not guess.isalpha() or not guess.islower():
+            print("Please, enter a lowercase letter from the English alphabet.")
+            continue
+        if guess in guessed_letters:
+            print("You've already guessed this letter.")
             continue
 
-        if not guess_letter.islower() or not guess_letter.isalpha():
-            print("Please enter a lowercase English letter")
-            continue
+        guessed_letters.add(guess)
 
-        if guess_letter in guessed_letters:
-            print("You've already guessed this letter")
-            continue
-
-        guessed_letters.add(guess_letter)
-
-        if guess_letter in secret_word:
-
-            display_word = "".join(
-                [c if c == guess_letter or c in guessed_letters or i < 3 else "-"
-                 for i, c in enumerate(secret_word)])
-
-            print(display_word)
+        if guess in secret_word:
+            new_display = ""
+            for i in range(len(secret_word)):
+                if secret_word[i] == guess:
+                    new_display += guess
+                else:
+                    new_display += display_word[i]
+            display_word = new_display
         else:
             print("That letter doesn't appear in the word")
             attempts -= 1
-            print(f"Attempts left: {attempts}")
 
-    if "-" not in display_word:
-        print(f"You guessed the word {secret_word}! You survived!")
+    if display_word == secret_word:
+        print(f"You guessed the word {secret_word}!")
+        print("You survived!")
     else:
         print("You lost!")
+
+
+print("HANGMAN")
+while True:
+    choice = input('Type "play" to play the game, "exit" to quit: > ').strip()
+
+    if choice == "play":
+        play_game()
+    elif choice == "exit":
+        break
+    else:
+        continue
