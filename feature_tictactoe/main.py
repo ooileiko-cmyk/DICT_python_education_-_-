67692fd5636217ef45ba2bb508dab90b05bafd45
ooileiko-cmyk <o.o.ileiko@student.khai.edu>
2025-11-22 17:36:00 +0200

    Этап 1: Создано игровое поле и функция его вывода

---
+++ feature_tictactoe/main.py
@@ -0,0 +1,8 @@
+board = [[" " for _ in range(3)] for _ in range(3)]
+
+def print_board():
+    print("  0 1 2")
+    for i, row in enumerate(board):
+        print(i, " ".join(row))
+
+print_board()
