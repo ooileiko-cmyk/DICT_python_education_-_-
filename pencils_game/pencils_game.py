159c2bf4bb45173543d1be61bb0d392461090cc8
ooileiko-cmyk <o.o.ileiko@student.khai.edu>
2025-11-23 16:35:37 +0200

    Stage 1: basic input for pencils game

---
+++ pencils_game/pencils_game.py
@@ -0,0 +1,12 @@
+pencils = input("How many pencils would you like to use:\n> ")
+pencils = int(pencils)
+
+
+user = "John"
+bot = "Jack"
+
+first = input(f"Who will be the first ({user}, {bot}):\n> ")
+
+
+print("|" * pencils)
+print(f"{first} is going first!")
