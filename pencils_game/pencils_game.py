0e1ab282f48e72e686a8960f9033803f0c04c90e
ooileiko-cmyk <o.o.ileiko@student.khai.edu>
2025-11-23 16:48:13 +0200

    Stage 3: add input validation and winner detection

--- pencils_game/pencils_game.py
+++ pencils_game/pencils_game.py
@@ -1,26 +1,41 @@
 pencils = input("How many pencils would you like to use:\n> ")
-pencils = int(pencils)
 
+pencils = int(pencils)
 
 user = "John"
 bot = "Jack"
 
 first = input(f"Who will be the first ({user}, {bot}):\n> ")
 
-
 print("|" * pencils)
 print(f"{first} is going first!")
+
 current = first
 
 while pencils > 0:
     print("|" * pencils)
     print(f"{current}'s turn:")
 
-    taken = int(input("> "))
+    while True:
+        take = input("> ")
 
-    pencils -= taken
+        if take not in ["1", "2", "3"]:
+            print("Possible values: '1', '2' or '3'")
+            continue
+
+        take = int(take)
+
+        if take > pencils:
+            print("Too many pencils were taken")
+            continue
+
+        break
+
+    pencils -= take
 
     if pencils == 0:
+        winner = bot if current == user else user
+        print(f"{winner} won!")
         break
 
     current = bot if current == user else user
