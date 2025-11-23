9130448b99aaf9b470b3924ff17c0cfad9355433
ooileiko-cmyk <o.o.ileiko@student.khai.edu>
2025-11-23 16:37:45 +0200

    Stage 2: basic game loop for pencils game

--- pencils_game/pencils_game.py
+++ pencils_game/pencils_game.py
@@ -10,3 +10,17 @@ first = input(f"Who will be the first ({user}, {bot}):\n> ")
 
 print("|" * pencils)
 print(f"{first} is going first!")
+current = first
+
+while pencils > 0:
+    print("|" * pencils)
+    print(f"{current}'s turn:")
+
+    taken = int(input("> "))
+
+    pencils -= taken
+
+    if pencils == 0:
+        break
+
+    current = bot if current == user else user
