20b7815319841073daf425fb1ef170e7f8a5bc84
ooileiko-cmyk <o.o.ileiko@student.khai.edu>
2026-02-24 14:32:25 +0200

    Stage 3: loop, exit and invalid input

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -23,3 +23,28 @@ elif (user_choice == "rock" and computer_choice == "scissors") or \
     print(f"Well done. The computer chose {computer_choice} and failed")
 else:
     print(f"Sorry, but the computer chose {computer_choice}")
+import random
+
+options = ["rock", "paper", "scissors"]
+
+while True:
+    user_choice = input()
+
+    if user_choice == "!exit":
+        print("Bye!")
+        break
+
+    if user_choice not in options:
+        print("Invalid input")
+        continue
+
+    computer_choice = random.choice(options)
+
+    if user_choice == computer_choice:
+        print(f"There is a draw ({computer_choice})")
+    elif (user_choice == "rock" and computer_choice == "scissors") or \
+         (user_choice == "paper" and computer_choice == "rock") or \
+         (user_choice == "scissors" and computer_choice == "paper"):
+        print(f"Well done. The computer chose {computer_choice} and failed")
+    else:
+        print(f"Sorry, but the computer chose {computer_choice}")
