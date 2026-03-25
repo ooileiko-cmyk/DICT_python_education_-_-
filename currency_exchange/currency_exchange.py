502f647a1321baa69fa77b6d64abd0f93cee0736
ooileiko-cmyk <o.o.ileiko@student.khai.edu>
2026-03-25 13:51:41 +0200

    Этап 2: конвертация mycoins в несколько валют

--- currency_exchange/currency_exchange.py
+++ currency_exchange/currency_exchange.py
@@ -8,3 +8,24 @@ def stage1():
 if name == "main":
     print("=== Этап 1 ===")
     stage1()
+
+
+    # Этап 2: конвертация в несколько валют
+    def stage2():
+        mycoin = float(input("Введите количество ваших mycoins: "))
+
+        rates = {
+            "HNL": 0.17,
+            "ARS": 0.82,
+            "MAD": 0.208,
+            "AUD": 1.9622
+        }
+
+        for currency, rate in rates.items():
+            total = round(mycoin * rate, 2)
+            print(f"Вы получите {total} {currency} от продажи {mycoin} mycoins.")
+
+
+    if name == "main":
+        print("=== Этап 2 ===")
+        stage2()
